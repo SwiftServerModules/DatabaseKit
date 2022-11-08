6263084760c8b80ddd2533d8f3d47faa8a47998b
Vivek TOPIYA <vivektopiy12@gmail.com>
2022-11-09 02:33:55 +0530

    updated with driverType

--- Sources/DatabaseKit/DataSource/DriverManagerDataSource.swift
+++ Sources/DatabaseKit/DataSource/DriverManagerDataSource.swift
@@ -1,11 +1,17 @@
+import Logging
 import SQLBaseKit
 
 public class DriverManagerDataSource {
+
+  let logger: Logger = Logger(label: "DriverManagerDataSource")
+
   public var url: String?
 
   public var username: String?
 
   public var password: String?
+
+  public var dialect: Dialect?
 
   public var properties: [String: String] = [:]
 
@@ -30,6 +36,10 @@ public class DriverManagerDataSource {
   }
 
   func getConnectionFromDriverManager(url: String?, properties: [String: String]) -> Connection {
-    return DriverManager.getConnection(url: url, properties: properties)
+    guard let driverType = dialect else {
+      fatalError("Dialect is not set")
+    }
+    logger.info("Getting Driver for \(driverType.rawValue)")
+    return DriverManager.getConnection(url: url, driver: driverType, properties: properties)
   }
 }
